658f1fb35d7c932751649a7becb1b5f12cb17839
uixc <sourav.sen84@gmail.com>
2017-09-16 14:06:35 +0530

    206

---
+++ component_demos/image_slider_style1_v1.0/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	$('.slider-inner img').addClass('fade');
+	$('.slider-inner img:first').addClass('active');		
+
+	$('.next').on('click', function() {
+		var currentImg = $('.active');
+		var nextImg = $('.active').next();		
+
+		if (nextImg.length == true) {
+			currentImg.removeClass('active')	;
+			nextImg.addClass('active');
+		}
+
+		if (nextImg.length == false) {
+			currentImg.removeClass('active')	;
+			$('.slider-inner img:first').addClass('active');
+		}
+	});
+
+	$('.prev').on('click', function() {
+		var currentImg = $('.active');
+		var prevImg = $('.active').prev();
+
+		if (prevImg.length == true) {
+			currentImg.removeClass('active')	;
+			prevImg.addClass('active');
+		}
+
+		if (prevImg.length == false) {
+			currentImg.removeClass('active')	;
+			$('.slider-inner img:last').addClass('active');
+		}		
+	});
+
+	var auto = function() {
+		var currentImg = $('.active');
+		var nextImg = $('.active').next();
+
+		if (nextImg.length == true) {
+			currentImg.removeClass('active')	;
+			nextImg.addClass('active');
+		}
+		else {
+			currentImg.removeClass('active')	;
+			$('.slider-inner img:first').addClass('active');
+		}
+	}
+	setInterval(auto, 3000);
+});
